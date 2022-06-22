c621eeb244aceb9dd5a29acec17f9788d8e56f24
kaisic1224 <89894485+kaisic1224@users.noreply.github.com>
2022-06-21 23:15:25 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "black-secondary": "#0C0717",
+        "purple-black": "#150050",
+        "purple-primary": "#3F0071",
+        "purple-secondary": "#610094",
+        "purple-accent": "#890fed",
+        cream: "#fffdc0"
+      }
+    }
+  },
+  plugins: []
+};
